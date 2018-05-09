35f624160bd1831790f936dbe0d9113dd355228b
Papatiara <shirazipatricia@gmail.com>
2018-05-09 14:37:58 -0700

    React - Solo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// TODO
+
+var GroceryList = (props) => (
+    <ul> 
+        <li>{props.buy[0]}</li>
+        <li>{props.buy[1]}</li>
+   </ul>
+)
+
+  var App = () => (
+    <div>
+        <h1> Grocery List </h1>
+        <GroceryList buy={['Beans', 'Eggs']}/>
+    </div>
+  );
+
+
+//<GroceryList buy={['Beans', 'Eggs']}/>
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+
